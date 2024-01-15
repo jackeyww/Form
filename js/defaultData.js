64a638868e711a7c044c937a47d0624192fbd4ec
jackeyww <44151849+jackeyww@users.noreply.github.com>
2024-01-16 06:50:02 +0800

    Add files via upload

---
+++ js/defaultData.js
@@ -0,0 +1,524 @@
+var defaultData = {
+	// 解冻冰箱
+	unfreezer: [{
+			key: 57,
+			name: "扣肉",
+			value: 0,
+		},
+		{
+			key: 62,
+			name: "小炒肉",
+			value: 0,
+		},
+		{
+			key: 55,
+			name: "风干牛肉",
+			value: 0,
+		},
+		{
+			key: 49,
+			name: "竹笋鸡翅",
+			value: 0,
+		},
+		{
+			key: 90,
+			name: "河虾",
+			value: 0,
+		},
+		{
+			key: 93,
+			name: "调理肉片",
+			value: 0,
+		},
+		{
+			key: 97,
+			name: "辣子鸡",
+			value: 0,
+		},
+		{
+			key: 71,
+			name: "瘦肉块",
+			value: 0,
+		},
+		{
+			key: 73,
+			name: "岭南鸡块",
+			value: 0,
+		},
+		{
+			key: 72,
+			name: "菌菇包",
+			value: 0,
+		},
+		{
+			key: 61,
+			name: "鸡汤面杂",
+			value: 0,
+		},
+		{
+			key: 69,
+			name: "毛豆烧土鸡",
+			value: 0,
+		},
+		{
+			key: 31,
+			name: "皮冻",
+			value: 0,
+		},
+		{
+			key: 4,
+			name: "咸鸡油",
+			value: 0,
+		},
+		{
+			key: 60,
+			name: "牛肉面牛肉",
+			value: 0,
+		},
+		{
+			key: 70,
+			name: "巴沙鱼",
+			value: 0,
+		},
+		{
+			key: 48,
+			name: "鸡汁辣鱼",
+			value: 0,
+		},
+		{
+			key: 58,
+			name: "红烧鸡杂",
+			value: 0,
+		},
+		{
+			key: 75,
+			name: "小母鸡",
+			value: 0,
+		},
+		{
+			key: 76,
+			name: "肥西老母鸡",
+			value: 0,
+		},
+		{
+			key: 74,
+			name: "老母鸡",
+			value: 0,
+		},
+		{
+			key: 83,
+			name: "咸鸭爪",
+			value: 0,
+		},
+		{
+			key: 89,
+			name: "员工餐",
+			value: 0,
+		},
+	],
+	//冷藏冰箱
+	refrigerate: [{
+			key: 53,
+			name: "老豆腐",
+			value: 0,
+		},
+		{
+			key: 41,
+			name: "红椒",
+			value: 0,
+		},
+		{
+			key: 92,
+			name: "小米辣",
+			value: 0,
+		},
+		{
+			key: 47,
+			name: "香菇",
+			value: 0,
+		}, {
+			key: 96,
+			name: "木耳",
+			value: 0,
+		},
+		{
+			key: 95,
+			name: "蒜花",
+			value: 0,
+		},
+		{
+			key: 91,
+			name: "青椒丁",
+			value: 0,
+		},
+		{
+			key: 94,
+			name: "胡萝卜片",
+			value: 0,
+		},
+		{
+			key: 46,
+			name: "胡萝卜丁",
+			value: 0,
+		},
+		{
+			key: 50,
+			name: "干丝",
+			value: 0,
+		},
+		{
+			key: 9,
+			name: "茄子料",
+			value: 0,
+		},
+		{
+			key: 43,
+			name: "生姜",
+			value: 0,
+		},
+		{
+			key: 44,
+			name: "蒜子",
+			value: 0,
+		},
+		{
+			key: 45,
+			name: "方干",
+			value: 0,
+		},
+		{
+			key: 54,
+			name: "白干",
+			value: 0,
+		},
+		{
+			key: 40,
+			name: "炼制鸡油",
+			value: 0,
+		},
+		{
+			key: 59,
+			name: "牛肉汤底",
+			value: 0,
+		},
+		{
+			key: 102,
+			name: "河虾料",
+			value: 0,
+		},
+		{
+			key: 52,
+			name: "豆腐料",
+			value: 0,
+		},
+		{
+			key: 63,
+			name: "小炒汁",
+			value: 0,
+		},
+		{
+			key: 99,
+			name: "鸡翅料",
+			value: 0,
+		},
+		{
+			key: 100,
+			name: "辣鱼料",
+			value: 0,
+		},
+		{
+			key: 81,
+			name: "手擀面",
+			value: 0,
+		},
+	],
+	//冷冻冰箱
+	freezer: [{
+			key: 16,
+			name: "馄饨",
+			value: 0,
+		},
+		{
+			key: 24,
+			name: "米饺",
+			value: 0,
+		},
+		{
+			key: 15,
+			name: "小笼包",
+			value: 0,
+		},
+		{
+			key: 21,
+			name: "香菇菜包",
+			value: 0,
+		},
+		{
+			key: 19,
+			name: "海带包",
+			value: 0,
+		},
+		{
+			key: 20,
+			name: "肉包",
+			value: 0,
+		},
+		{
+			key: 22,
+			name: "春卷",
+			value: 0,
+		},
+		{
+			key: 25,
+			name: "烧卖",
+			value: 0,
+		},
+		{
+			key: 18,
+			name: "蒸饺",
+			value: 0,
+		},
+		{
+			key: 17,
+			name: "小鸡包",
+			value: 0,
+		},
+		{
+			key: 98,
+			name: "馒头",
+			value: 0,
+		},
+		{
+			key: 23,
+			name: "油条",
+			value: 0,
+		},
+		{
+			key: 34,
+			name: "棒棒虾",
+			value: 0,
+		},
+		{
+			key: 27,
+			name: "意大利面",
+			value: 0,
+		},
+		{
+			key: 39,
+			name: "宝宝鸡翅",
+			value: 0,
+		},
+		{
+			key: 30,
+			name: "鸡丝",
+			value: 0,
+		},
+		{
+			key: 38,
+			name: "上汤汁",
+			value: 0,
+		},
+		{
+			key: 33,
+			name: "炸鸡排",
+			value: 0,
+		},
+		{
+			key: 32,
+			name: "薯饼",
+			value: 0,
+		},
+		{
+			key: 26,
+			name: "炸鸡腿",
+			value: 0,
+		},
+		{
+			key: 33,
+			name: "炸鸡腿",
+			value: 0,
+		},
+		{
+			key: 29,
+			name: "鸡汤料",
+			value: 0,
+		},
+		{
+			key: 36,
+			name: "卤翅根",
+			value: 0,
+		},
+		{
+			key: 37,
+			name: "卤肉肠",
+			value: 0,
+		},
+		{
+			key: 35,
+			name: "卤鸡腿",
+			value: 0,
+		},
+	],
+	//单门冰箱
+	single: [{
+			key: 7,
+			name: "豆米",
+			value: 0,
+		},
+		{
+			key: 28,
+			name: "西兰花",
+			value: 0,
+		},
+		{
+			key: 8,
+			name: "红烧茄子",
+			value: 0,
+		},
+		{
+			key: 10,
+			name: "南瓜丁",
+			value: 0,
+		},
+		{
+			key: 101,
+			name: "梅菜",
+			value: 0,
+		},
+		{
+			key: 11,
+			name: "蒸蛋虾仁",
+			value: 0,
+		},
+		{
+			key: 12,
+			name: "开背虾",
+			value: 0,
+		},
+		{
+			key: 14,
+			name: "卤肉",
+			value: 0,
+		},
+		{
+			key: 13,
+			name: "黄焖鸡",
+			value: 0,
+		},
+	],
+	//素菜
+	vegetable: [{
+			key: 5,
+			name: "麻辣油",
+			value: 0,
+		},
+		{
+			key: 6,
+			name: "葱油料",
+			value: 0,
+		},
+		{
+			key: 64,
+			name: "螺丝椒",
+			value: 0,
+		},
+		{
+			key: 51,
+			name: "大白菜",
+			value: 0,
+		},
+		{
+			key: 42,
+			name: "京葱",
+			value: 0,
+		},
+		{
+			key: 79,
+			name: "香葱",
+			value: 0,
+		},
+		{
+			key: 80,
+			name: "青菜",
+			value: 0,
+		},
+		{
+			key: 66,
+			name: "西红柿",
+			value: 0,
+		},
+		{
+			key: 65,
+			name: "娃娃菜",
+			value: 0,
+		},
+		{
+			key: 67,
+			name: "包菜",
+			value: 0,
+		},
+		{
+			key: 68,
+			name: "乌心菜",
+			value: 0,
+		},
+		{
+			key: 85,
+			name: "玉米",
+			value: 0,
+		},
+		{
+			key: 86,
+			name: "蜜薯",
+			value: 0,
+		},
+		{
+			key: 87,
+			name: "南瓜",
+			value: 0,
+		},
+		{
+			key: 1,
+			name: "土鸡蛋",
+			value: 0,
+		},
+		{
+			key: 2,
+			name: "咸鸭蛋",
+			value: 0,
+		},
+		{
+			key: 3,
+			name: "鸡蛋干",
+			value: 0,
+		},
+		{
+			key: 88,
+			name: "咸豆角",
+			value: 0,
+		},
+	],
+	//熟品冰箱
+	Cooked:[
+		{
+			key: 77,
+			name: "卤鸡蛋",
+			value: 0,
+		},
+		{
+			key: 82,
+			name: "菜根香",
+			value: 0,
+		},
+		{
+			key: 84,
+			name: "猪油",
+			value: 0,
+		},
+		{
+			key: 78,
+			name: "蛋皮",
+			value: 0,
+		},
+	]
+}
